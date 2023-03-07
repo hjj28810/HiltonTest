13184fe5572128996bcd8dfab12be4b37f2b59d4
Alvin Huang <huangjunjie@lanyife.com>
2023-03-07 10:49:45 +0800

    增加login  test

--- server/src/test/app.test.js
+++ server/src/test/app.test.js
@@ -6,27 +6,51 @@ const should = require('should')
 
 request = supertest(app);
 describe('app js test', function () {
-    describe('GET /', function () {
-        it('should respond to GET with reservations', function (done) {
+    var token = ""
+    describe('POST /', function () {
+        it('should respond to POST with login', function (done) {
             this.timeout(10000);
             setTimeout(() => {
                 var nonce = uuidv4();
                 var curTime = Math.floor(new Date().getTime() / 1000);
-                request.get('/api/v1/reservations')
-                    .query({ guest_id: '13321952950' })
+                request.post('/api/v1/login')
+                    .send({ id: '13321952950' })
                     .set('Content-Type', 'application/json')
                     .set('Nonce', nonce)
                     .set('CurTime', curTime)
                     .set('CheckSum', cryptoJS.SHA1(`6f2ad63f0ef2${nonce}${curTime}`).toString(cryptoJS.enc.Hex))
-                    //去掉jwt验证，在app.js 增加credentialsRequired: false
-                    .set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkVtcGxveWVlIiwiaWF0IjoxNjc4MTUyNDk0LCJleHAiOjE2NzgxNTk2OTR9.SmQQc1EaS_hZBRITu9WXgS3QiOn0MikbYY0_iGzQeLQ')
                     .expect(200)
                     .end(function (err, res) {
                         should.not.exist(err);
                         should.exist(res.body.data)
+                        token = res.body.data.token
                         done()
                     });
             }, 2000);
+        });
+    });
+
+    describe('GET /', function () {
+        it('should respond to GET with reservations', function (done) {
+            // this.timeout(10000);
+            // setTimeout(() => {
+            var nonce = uuidv4();
+            var curTime = Math.floor(new Date().getTime() / 1000);
+            request.get('/api/v1/reservations')
+                .query({ guest_id: '13321952950' })
+                .set('Content-Type', 'application/json')
+                .set('Nonce', nonce)
+                .set('CurTime', curTime)
+                .set('CheckSum', cryptoJS.SHA1(`6f2ad63f0ef2${nonce}${curTime}`).toString(cryptoJS.enc.Hex))
+                //去掉jwt验证，在app.js 增加credentialsRequired: false
+                .set('Authorization', token)
+                .expect(200)
+                .end(function (err, res) {
+                    should.not.exist(err);
+                    should.exist(res.body.data)
+                    done()
+                });
+            // }, 2000);
         });
     });
 
@@ -44,7 +68,7 @@ describe('app js test', function () {
                 .set('CurTime', curTime)
                 .set('CheckSum', cryptoJS.SHA1(`6f2ad63f0ef2${nonce}${curTime}`).toString(cryptoJS.enc.Hex))
                 //去掉jwt验证，在app.js 增加credentialsRequired: false
-                .set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IkVtcGxveWVlIiwiaWF0IjoxNjc4MTUyNDk0LCJleHAiOjE2NzgxNTk2OTR9.SmQQc1EaS_hZBRITu9WXgS3QiOn0MikbYY0_iGzQeLQ')
+                .set('Authorization', token)
                 .expect(200)
                 .end(function (err, res) {
                     should.not.exist(err);
